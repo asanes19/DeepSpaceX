cb016c363cf2c5afe7d42fd654b45b0fe5edaf97
absherif19 <abdelrahman.gamal@psinv.net>
2025-05-16 09:25:36 +0400

    hero done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import HeaderOne from './components/Headers/HeaderOne';
+import Hero from './components/hero/Hero';
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <HeaderOne />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
